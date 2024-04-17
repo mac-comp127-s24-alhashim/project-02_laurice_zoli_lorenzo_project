67f16234bbc67eccf2c0e1e9489da5b9115ef3fe
zlauko5 <zolblauk5@gmail.com>
2024-04-17 10:43:23 -0500

    fix

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -15,9 +15,7 @@ public class SnakeGame {
         CanvasWindow canvas = new CanvasWindow("Snake", boardWidth, boardHeight);
         canvas.setBackground(Color.BLACK);
         canvas.draw();
-        addSnakeHead(canvas);
-        addFood(canvas);
-        addGrid(canvas);
+        draw(canvas);
     }
     public static void main(String[] args) {
         SnakeGame game = new SnakeGame(600, 600);
@@ -45,27 +43,7 @@ public class SnakeGame {
 
     }
 
-    public static void addGrid(CanvasWindow canvas){
-        for(int i = 0; i < boardWidth/TILE_SIZE; i++){
-            Line vertical = new Line(i * TILE_SIZE, 0, i * TILE_SIZE, boardHeight);
-            Line horizontal = new Line(0, i * TILE_SIZE, boardWidth, i * TILE_SIZE);
-            vertical.setStrokeColor(Color.GRAY);
-            horizontal.setStrokeColor(Color.GRAY);
-            canvas.add(vertical);
-            canvas.add(horizontal);
-        }
-    }
-
-
-    public static void addSnakeHead(CanvasWindow canvas){
-        Tile snakeHead = new Tile(5 * TILE_SIZE, 5 * TILE_SIZE, 25, Color.GREEN);
-        snakeHead.addToCanvas(canvas);
-    }
-
-    public static void addFood(CanvasWindow canvas){
-        Tile food = new Tile(10 * TILE_SIZE, 10 * TILE_SIZE, TILE_SIZE, Color.RED);
-        food.addToCanvas(canvas);
-    }
+    
 
 
 }
